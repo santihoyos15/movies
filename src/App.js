812ed6158c57a6cb06600dbe40cdec5ea1b8dbae
santihoyos15 <santiagohoyos2808@gmail.com>
2022-05-17 16:05:36 -0500

    Movie gallery UI design implemented.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './App.css';
 import SearchBox from './components/SearchBox'
 import Movie from './components/Movie'
+import CategoryList from './components/CategoryList'
 import {API_KEY} from './secrets.js'
 
 export default function App() {
-    const [inputValue, setInputValue] = React.useState('')
+    const [search, setSearch] = React.useState('')
 
     const moviesElements = [
         <Movie></Movie>,
@@ -14,14 +15,14 @@ export default function App() {
     ]
 
     function handleChange (evt) {
-        setInputValue(evt.target.value);
+        setSearch(evt.target.value);
     }
 
 	return (
         <div className="App">
             <main className="landing-page-container">
                 <h1>Movies</h1>
-                <SearchBox handleChange={handleChange} value={inputValue}/>
+                <SearchBox handleChange={handleChange} value={search}/>
                 <div style={{display: 'flex', justifyContent: 'space-between'}}>
                     <h2>Trends</h2>
                     <h3>See more</h3>
@@ -29,7 +30,9 @@ export default function App() {
                 <div className="movie-gallery">
                     {moviesElements}
                 </div>
+                <h2>Categories</h2>
+                <CategoryList />
             </main>
         </div>
-    );
+    )
 }
---
+++ src/components/CategoryList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function CategoryList (props) {
+    // const categoriesElements = props.categories.map(category => <li>{category.name}</li>);
+    return (
+        <ul className='categories'>
+            <li><div></div><h3>Action</h3></li>
+            <li><div></div><h3>Action</h3></li>
+            <li><div></div><h3>Action</h3></li>
+            <li><div></div><h3>Action</h3></li>
+            <li><div></div><h3>Action</h3></li>
+            <li><div></div><h3>Action</h3></li>
+        </ul>
+    )
+}
